69cb8026e36d4d33d3cc830a0817daaefe5fc8cf
Ibtisam Mirza <iuhmirza@gmail.com>
2025-07-23 10:48:39 +0100

    Complete up to error handling

--- exercises/13_error_handling/errors4.rs
+++ exercises/13_error_handling/errors4.rs
@@ -7,10 +7,16 @@ enum CreationError {
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
+use std::cmp::Ordering;
+
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<Self, CreationError> {
         // TODO: This function shouldn't always return an `Ok`.
-        Ok(Self(value as u64))
+        match value.cmp(&0) {
+            Ordering::Less => Err(CreationError::Negative),
+            Ordering::Equal => Err(CreationError::Zero),
+            Ordering::Greater => Ok(Self(value as u64))
+        }
     }
 }
 
